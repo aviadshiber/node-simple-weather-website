c7ada6adf3080aa71b9dacfecddde6cb53a48b5d
aviadsh <Aviad.Shiber@amdocs.com>
2019-12-29 09:51:26 +0200

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+console.log('Client side javascript file is loaded!')
+
+
+const weatherForm=document.querySelector('form')
+const searchBox=document.querySelector('input')
+const message1=document.querySelector('#msg-1')
+const message2=document.querySelector('#msg-2')
+
+weatherForm.addEventListener('submit',(e) => {
+    e.preventDefault()
+    const location=searchBox.value
+    fetch('http://localhost:3000/weather?address='+location).then((response)=> {
+        response.json().then((data) =>{
+            if(data.error){
+                message2.textContent=data.error
+            }else{
+                message1.textContent=data.location+"\n"+data.forecast
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+            
+        })
+
+    })
+})
---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYXZpYWRzaCIsImEiOiJjazRpM3A2ZHgxMm9oM2txd3hiaDVyZzBnIn0.NNdMwXSQ7hIC_QL4Ew-FaQ&limit=1'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
